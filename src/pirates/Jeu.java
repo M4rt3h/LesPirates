37f5e861a1b3150b38be3393024cddbef09ab9b3
M4rt3h <martin.leteurtre@univ-tlse3.fr>
2025-03-12 20:10:04 +0100

    ajout fct verifier partie finie

--- src/pirates/Jeu.java
+++ src/pirates/Jeu.java
@@ -3,6 +3,8 @@ package pirates;
 import java.util.Random;
 
 public class Jeu {
+	private Personnage joueur1;
+	private Personnage joueur2;
 	private int nbCartes = 60;
 	private Carte[] pioche;;
 
@@ -22,8 +24,6 @@ public class Jeu {
 		}
 	}
 
-	// int randomNumberInRange = random.nextInt((max - min) + 1) + min;
-
 	public Carte piocherCarte() {
 		if (nbCartes > 0) {
 			return pioche[--nbCartes]; // Retourne la dernière carte de la pioche et décrémente le compteur
@@ -36,13 +36,17 @@ public class Jeu {
 		pioche[nbCartes++] = carte; // Ajoute la carte à la fin de la pioche et incrémente le compteur
 	}
 
+	private boolean jeuTerminé() {
+		return joueur1.getPV() <= 0 || joueur2.getPV() <= 0 || joueur1.getPopularite() >= 5
+				|| joueur2.getPopularite() >= 5;
+	}
+
 	public Personnage choisirPremierJoueur(Personnage pirateBill, Personnage pirateJack) {
 		Random random = new Random();
 		int randomNumber = random.nextInt(2);
 		if (randomNumber == 0) {
 			return pirateBill;
-		}
-		else {
+		} else {
 			return pirateJack;
 		}
 	}
