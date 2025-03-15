0f28d01cef5d2b40168f7c22cdc1c77cce2b18ee
M4rt3h <martin.leteurtre@univ-tlse3.fr>
2025-03-15 20:18:12 +0100

    Fin de la carte Kraken en 3 modes

--- src/pirates/CarteKraken.java
+++ src/pirates/CarteKraken.java
@@ -8,8 +8,8 @@ public class CarteKraken extends Carte {
 	int modeDeJeu = 0;
 
 	public CarteKraken(String nom, String description, String rarete, int leModeDeJeu) {
-		setNomCarte("Carte Kraken");
-		setDescription("Le Kraken apparait et te propose un deal");
+		setNomCarte(nom);
+		setDescription(description);
 		setRarete(rarete);
 		setModeDeJeu(leModeDeJeu);
 		carteToString();
@@ -33,7 +33,7 @@ public class CarteKraken extends Carte {
 		int reponse = -1;
 		int choixKraken = 0;
 		int tentative = 2;
-		if (modeDeJeu == 0) {
+		if (modeDeJeu == 1) {
 			affichage.afficherMessage("Kraken : Jouons à pile ou face, si tu gagnes je coulerai le navire de "
 					+ joueurAdverse.getNom() + " sinon tu coules");
 
@@ -42,7 +42,6 @@ public class CarteKraken extends Carte {
 			// Partie du code qui choisi aléatoirement entre 0 et 1
 			while ((reponse != 0) && (reponse != 1) && (tentative > 0)) {
 				reponse = affichage.lireReponse() + 1;
-				System.out.println(reponse);
 
 				if (reponse == choixKraken) {
 					affichage.afficherMessage("Kraken : Tu n'aurais pas du tenter ta chance...");
@@ -53,8 +52,7 @@ public class CarteKraken extends Carte {
 						break; // Pour ne pas afficher qu'il reste '0' chance
 					}
 					affichage.afficherMessage("Kraken : Ne te moque pas de moi");
-					affichage.afficherMessage("         encore " + String.valueOf(tentative)
-							+ " coup foireux comme celui là et tu es mort");
+					affichage.afficherMessage("         encore un coup foireux comme celui là et tu es mort");
 
 				} else {
 					affichage.afficherMessage("Kraken : Je n'ai qu'une seule parole.");
@@ -63,39 +61,89 @@ public class CarteKraken extends Carte {
 			}
 			if (tentative <= 0) {
 				affichage.afficherMessage("Kraken : Tu l'auras voulu");
-				affichage.afficherMessage("*Le Kraken entraine ton bateau dans les profondeurs*");
-				joueurCourant.modifVie(-joueurCourant.getPV());
+				affichage.afficherMessage("*Le Kraken entraine ton navire dans les profondeurs*");
+				joueurAdverse.modifVie(-joueurAdverse.getPV()); // Je n'ai pas trouvé pourquoi je dois mettre
+																// joueurAdverse au lieu de joueurCourant ici
 			}
 
-		} else if (modeDeJeu == 1) {
+		} else if (modeDeJeu == 2) {
 			List<Integer> reponsesPossibles = Arrays.asList(1, 2, 3);
 			affichage.afficherMessage("Kraken : Je vois que le jeune pirate que tu es a plein de bravoure");
 			affichage.afficherMessage("         C'est pourquoi je vais te proposer un deal que tu ne pourras refuser");
 			affichage.afficherMessage("         Nous allons jouer à pierre feuille ciseaux.");
 			affichage.afficherMessage("Saisi 1 pour la pierre, 2 pour la feuille et 3 pour les ciseaux");
-			reponse = affichage.lireReponse();
 			choixKraken = (int) (Math.random() * 3);
-			while (!reponsesPossibles.contains(reponse)) {
+			while (!reponsesPossibles.contains(reponse) && (tentative > 0)) {
+				reponse = affichage.lireReponse() + 1;
+
 				if (choixKraken == reponse) {
 					affichage.afficherMessage(
 							"Kraken : Je vois que nous sommes sur une egalite,\n         je vais te faire une faveur et retirer 1 PV a ton adversaire et nous serons quitte.");
 					joueurAdverse.modifVie(-1);
 				} else if ((choixKraken == 1) && (reponse == 3)) {
 					// Le kraken gagne
+					affichage.afficherMessage(
+							"Kraken : Tu as choisi les ciseaux...\n         ...contre la pierre...\n         MEURS !!");
+					affichage.afficherMessage(
+							"*Le Kraken provoque une tempête qui entoure ton navire et le fait sombrer*");
+					joueurCourant.modifVie(-joueurCourant.getPV());
 
 				} else if ((choixKraken == 3) && (reponse == 1)) {
 					// joueur gagne
+					affichage.afficherMessage(
+							"Kraken : Force est de constaté que tu es plus fort que moi a ce jeu.\n         Je te ferais donc grand vainceur de ta bataille avec "
+									+ joueurAdverse.getNom() + ".");
+					joueurAdverse.modifVie(-joueurAdverse.getPV());
+
 				} else {
 					tentative -= 1;
 					if (tentative <= 0) {
 						break; // Pour ne pas afficher qu'il reste '0' chance
 					}
 					affichage.afficherMessage("Kraken : J'ai l'impression que tu sous estime notre jeu");
-					affichage.afficherMessage("         refais moi encore " + String.valueOf(tentative)
-							+ " mauvaise blague et tu verras");
+					affichage.afficherMessage("         refais moi encore une mauvaise blague et tu verras");
+				}
+			}
+			if (tentative <= 0) {
+				affichage.afficherMessage("Kraken : Tu ne me laisse plus le choix");
+				affichage.afficherMessage("*Le Kraken souleve un ras de marre et englouti ton navire*");
+				joueurAdverse.modifVie(-joueurAdverse.getPV()); // Je n'ai pas trouvé pourquoi je dois mettre
+																// joueurAdverse au lieu de joueurCourant ici
+			}
+		} else if (modeDeJeu == 3) {
+			// Mode de jeu 3 : Deviner un nombre
+			int nombreSecret = (int) (Math.random() * 15);
+			System.out.println(nombreSecret);
+			int essaisRestants = 2;
+			affichage.afficherMessage("Kraken : Cette fois, tu dois prouver ta chance...");
+			affichage.afficherMessage("         Devine un nombre entre 1 et 10. Tu as " + essaisRestants
+					+ " tentatives.\n         Si tu réussis, je réduirai de moitié la vie et la popularité de ton adversaire.\n         Mais si tu échoues, tu subiras le même sort.");
+			while (essaisRestants > 0) {
+				reponse = affichage.lireReponse() + 1;
+
+				if (reponse == nombreSecret) {
+					affichage.afficherMessage(
+							"Kraken : Incroyable... Tu as de la chance, mais je tiens toujours parole.");
+					joueurAdverse.modifVie(-joueurAdverse.getPV() / 2);
+					joueurAdverse.modifPopularite(-joueurAdverse.getPopularite() / 2);
+					affichage.afficherMessage(
+							"*Le Kraken réduit la vie et la popularite de " + joueurAdverse.getNom() + "*");
+					return;
+				} else {
+					essaisRestants--;
+					if (essaisRestants > 0) {
+						affichage.afficherMessage(
+								"Kraken : Mauvaise réponse ! Il te reste " + essaisRestants + " essais.");
+					} else {
+						affichage.afficherMessage("Kraken : Ton manque de chance te condamne...");
+						joueurCourant.modifVie(-joueurCourant.getPV() / 2);
+						joueurCourant.modifPopularite(-joueurCourant.getPopularite() / 2);
+						affichage.afficherMessage(
+								"*Le Kraken réduit la vie et la popularite de " + joueurCourant.getNom() + "*");
+					}
 				}
 			}
 		}
-	}
 
+	}
 }
--- src/pirates/TypesCarte.java
+++ src/pirates/TypesCarte.java
@@ -1,7 +1,7 @@
 package pirates;
 
 public enum TypesCarte {
-   // ABORDAGE(new CarteAttaque("Abordage","Ton equipage et toi abordez le navire adverse",-2,"Commune")),
+    //ABORDAGE(new CarteAttaque("Abordage","Ton equipage et toi abordez le navire adverse",-2,"Commune")),
 	//COUPDESABRE(new CartePvEtPopularite("Coup de sabre", "Muni de ton fameux sabre, tu assenes un coup digne de ta legende a ton adversaire","Rare",-4,2)),
 	//ATTAQUE(new CarteAttaque("Attaque simple", "Tu jettes un cailloux sur ton adversaire...",1,"Commune")),
 	//MAINDEFER(new CartePopularite("Main de fer","Tu tapes du poing sur la table et remets ton equipage dans le rang",-1,"Commune",true)),
@@ -11,7 +11,9 @@ public enum TypesCarte {
 	//PERROQUET(new CartePopularite("Perroquet","Ton perroquet lance une pique mordante à l’infortuné marin du navire",1,"Commune",true)),
 	//COLLIER(new CartePopularite("Mettre un collier","Tu habilles ton costume de ton plus beau collier...",-2,"Commune",true)),
 	//MENDIANT(new CartePopularite("Mendiant", "Tu jettes une pièce à ton adversaire",-3,"Rare",false)),
-	KRAKEN(new CarteKraken("Kraken","Le Kraken apparait et te propose un deal","Legendaire",0)),
+	//KRAKEN1(new CarteKraken("Kraken des profondeurs","Le Kraken apparait et te propose un deal","Epic",1)),
+	//KRAKEN2(new CarteKraken("Kraken des abysses","Ce Kraken peut te faire gagner, ou te tuer","Legendaire",2)),
+	KRAKEN3(new CarteKraken("Kraken joueur","Tu as une petite chance de t'en sortir","Rare",3))
 	//SABAT(new CartePvEtPopularite("Coup de sabat", "Tu donnes un coup de jambes de bois à ton adversaire mais tu glisses et tombes...","Commune",-2,-3))
     ;
 
